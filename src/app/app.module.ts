42c7a6f5325f58cc38f90efdd101df244256dee6
Home <Home@Homes-iMac.local>
2019-04-23 18:42:24 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { TabsComponent } from './tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule
